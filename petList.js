ee42bbf205f2de63a8522bf9dcc9c891ab0ff78b
GannaKov <zlatta2000@gmail.com>
2024-03-17 22:41:38 +0100

    add pets

--- petList.js
+++ petList.js
@@ -17,6 +17,23 @@ const pets = {
         "Eager and curious, Shadow enjoys company and can always be found tagging along at your heels!",
       url: "https://placedog.net/540/205",
     },
+    {
+      id: "dog-uJA0mv",
+      name: "Catty",
+      age: 2,
+      breed: " Collie",
+      description:
+        "Catty curious,  and can always be found tagging along at your heels!",
+      url: "https://placedog.net/540/205?id=10",
+    },
+    {
+      id: "dog-uJA0mO",
+      name: "Baby",
+      age: 0,
+      breed: "Unknown",
+      description: "Baby is very very sweet !!!!",
+      url: "https://placedog.net/540/205?id=12",
+    },
   ],
   cats: [
     {
@@ -28,16 +45,62 @@ const pets = {
         "Snowflake is a playful kitten who loves roaming the house and exploring.",
       url: "http://placekitten.com/500/500",
     },
-  ],
-  rabbits: [
     {
-      id: "rab-GbrFGU",
+      id: "cat-uJA0mv",
+      name: "Ketty",
+      age: 1,
+      breed: "Tabby",
+      description: "Ketty is  loves roaming the house and exploring.",
+      url: "http://placekitten.com/500/500",
+    },
+    {
+      id: "cat-uJA0mv",
+      name: "Bobr",
+      age: 1,
+      breed: "Tabby",
+      description:
+        "Bobr is a playful kitten who loves roaming the house and exploring.",
+      url: "http://placekitten.com/500/500",
+    },
+    {
+      id: "cat-uJA0mv",
+      name: "Wiskey",
+      age: 1,
+      breed: "Tabby",
+      description:
+        "Wiskey is a playful kitten who loves roaming the house and exploring.",
+      url: "http://placekitten.com/500/500",
+    },
+  ],
+  hamsters: [
+    {
+      id: "hamster-GbrFGU",
       name: "Easter",
       age: 4,
       breed: "Mini Rex",
       description:
         "Easter is a sweet, gentle rabbit who likes spending most of the day sleeping.",
-      url: "https://loremflickr.com/320/240/rabbit?lock=7",
+      url: "https://loremflickr.com/320/240/hamster?lock=7",
+    },
+  ],
+  bears: [
+    {
+      id: "bear-GbrFGU",
+      name: "Kuki",
+      age: 3,
+      breed: "simply Bear",
+      description: "Kuki is a sweet, cute and sleep a lot. ",
+      url: "https://placebear.com/g/320/240",
+    },
+  ],
+  tigers: [
+    {
+      id: "tiger-GbrFGU",
+      name: "Murry",
+      age: 3,
+      breed: "tiger",
+      description: "Murry will be your friend for ever. ",
+      url: "https://picsum.photos/id/219/320/240",
     },
   ],
 };
